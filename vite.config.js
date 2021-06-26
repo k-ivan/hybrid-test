59a87aaca34e7922abd00404ac66bde57322470b
Ivan Kuzmichov <ivan.kuzmichov@gmail.com>
2021-06-26 20:43:17 +0300

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
 import path from 'path';
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import viteSvgIcons from 'vite-plugin-svg-icons';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/hybrid-test/',
-  plugins: [
-    vue(),
-    viteSvgIcons({
-      // Specify the icon folder to be cached
-      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-      // Specify symbolId format
-      symbolId: 'icon-[dir]-[name]',
-    })
-  ],
-  define: {
-    'process.env': {}
+export default ({ mode }) => {
+  return {
+    base: (mode === 'development') ? '/' : '/hybrid-test/',
+    plugins: [
+      vue(),
+      viteSvgIcons({
+        // Specify the icon folder to be cached
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        // Specify symbolId format
+        symbolId: 'icon-[dir]-[name]',
+      })
+    ],
+    define: {
+      'process.env': {}
+    }
   }
-})
+}
