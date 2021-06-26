4cdf770034a93636f53fb967ca884d158e2a9438
Ivan Kuzmichov <ivan.kuzmichov@gmail.com>
2021-06-26 20:34:04 +0300

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import viteSvgIcons from 'vite-plugin-svg-icons';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/hybrid-test/',
   plugins: [
     vue(),
     viteSvgIcons({
