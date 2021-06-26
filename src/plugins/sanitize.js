2491d6b1d9fae6162dc716a2b9a9b70920388e6f
k-ivan <ivan.kuzmichov@gmail.com>
2021-06-26 17:13:28 +0000

    Deploying to gh-pages from @ k-ivan/hybrid-test@dae73f6d4fec23622208ece78caed9c218715a9d 🚀

--- src/plugins/sanitize.js
+++
@@ -1,9 +0,0 @@
-import sanitizeHtml from 'sanitize-html';
-
-export default {
-  install: (app) => {
-    app.config.globalProperties.$sanitizeHtml = (dirty, options = {}) => {
-      return sanitizeHtml(dirty, options)
-    }
-  }
-}
